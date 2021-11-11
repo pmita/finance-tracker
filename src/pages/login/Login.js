a597f0d9f9199b8faad4c06ed7097b86f8527591
pmita <panos.mita2@outlook.com>
2021-11-11 17:37:49 +0000

    Feat/Initial setup, and file structures done

---
+++ src/pages/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Login.module.css';
+
+const Login = () => {
+    return(
+        <div>
+            <h1>Hello from Login Page component</h1>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Signup.module.css';
+
+const Signup = () => {
+    return(
+        <div>
+            <h1>Hello from Signup Page component</h1>
+        </div>
+    );
+}
+
+export default Signup;
