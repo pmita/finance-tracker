931b144d3bf0b353134224b75a70f97e3aaf32fd
pmita <panos.mita2@outlook.com>
2021-11-12 16:08:07 +0000

    Feature/Implementing useSignup hook

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import styles from './Signup.module.css';
+// HOOKS
+import { useSignup } from '../../hooks/useSignup';
 
 const Signup = () => {
     // STATE
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [displayName, setDisplayName] = useState('');
+    const { signup, isPending, error } = useSignup();
+
+    // EVENT HANDLERS
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        signup(email, password, displayName);
+    }
     return(
-        <form className={styles['login-form']} >
+        <form className={styles['login-form']} onSubmit={handleSubmit}>
             <h2>Login</h2>
             <label>
                 <span>Email:</span>
@@ -24,7 +34,17 @@ const Signup = () => {
                     value={password}
                 />
             </label>
-            <button className='btn'>Login</button>
+            <label>
+                <span>Display Name:</span>
+                <input 
+                    type='text' 
+                    onChange={(e) => setDisplayName(e.target.value)}
+                    value={displayName}
+                />
+            </label>
+            {!isPending && <button className='btn'>Sign up</button>}
+            {isPending && <button className='btn' disabled>loading...</button>}
+            {error && <p className='error'>{error}</p>}
         </form>
     );
 }
