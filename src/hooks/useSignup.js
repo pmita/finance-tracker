32b1c009dd0f4f60cf21cd3db8f906c0861c728d
pmita <panos.mita2@outlook.com>
2021-11-12 17:29:13 +0000

    FEature/Dispatching a login action

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 //FIREBASE
 import { projectAuth } from '../firebase/config';
+// HOOKS
+import { useAuthContext } from './useAuthContext';
 
 export const useSignup = () => {
     // STATE
     const [ error, setError ] = useState(null);
     const [isPending, setIsPending ] = useState(false);
+    const { dispatch } = useAuthContext();
 
     //FUNCTIONS
     const signup = async (email, password, displayName) => {
@@ -16,6 +19,7 @@ export const useSignup = () => {
             //sign up user
             const res = await projectAuth.createUserWithEmailAndPassword(email, password) // it sends a response
             console.log(res.user); // details about user just created
+            
 
             if(!res){
                 throw new Error('Could not complete sign up');
@@ -23,6 +27,10 @@ export const useSignup = () => {
 
             //add display name to user
             await res.user.updateProfile({ displayName : displayName}); //after user is created, update the display name for the user
+            
+            //dispatch login action
+            dispatch({ type : 'LOGIN', payload : res.user});
+            
             setIsPending(false);
             setError(null);
         } catch(err){
