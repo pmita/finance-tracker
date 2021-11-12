49cb7e29a118568c4277af70c03cb7338c5ae502
pmita <panos.mita2@outlook.com>
2021-11-12 14:47:58 +0000

    Feat/Routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css'
+// ROUTER
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// PAGES
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
