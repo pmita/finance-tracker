746032129144b175ce19c643bda37106bcc5fbd2
pmita <panos.mita2@outlook.com>
2021-11-12 15:06:31 +0000

    Feature/Login form page added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+// COMPONENTS
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Signup.module.css';
 
 const Signup = () => {
+    // STATE
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return(
-        <div>
-            <h1>Hello from Signup Page component</h1>
-        </div>
+        <form className={styles['login-form']} >
+            <h2>Login</h2>
+            <label>
+                <span>Email:</span>
+                <input 
+                    type='email' 
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input 
+                    type='password' 
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className='btn'>Login</button>
+        </form>
     );
 }
 
