18df4b4f47e951d530ad7ca2f21c804e33da5c90
pmita <panos.mita2@outlook.com>
2021-11-12 15:32:42 +0000

    Feasture/Configuring firebase with react

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDpMRG1HTRfpNB_dWQlMTL_t6k1zoEL-U4",
+    authDomain: "mymonay-2860c.firebaseapp.com",
+    projectId: "mymonay-2860c",
+    storageBucket: "mymonay-2860c.appspot.com",
+    messagingSenderId: "155262775673",
+    appId: "1:155262775673:web:496ab89a5481859af9bfa8"
+  };
+
+  //Init firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //Init services
+  const projectFirestore = firebase.firestore();
+
+  export { projectFirestore };
