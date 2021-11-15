150826e4bd72aa5b6a766e290d0a3519537b88a8
pmita <panos.mita2@outlook.com>
2021-11-15 11:36:07 +0000

    Feature/Quering transaction per user id

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,16 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 // FIREBASE
 import { projectFirestore } from '../firebase/config';
 
-export const useCollection = (collection) => {
-    //STATE
+export const useCollection = (collection, _query) => {
+    //STATE & VARIABLES
     const [documents, setDocuments] = useState(null);
     const [error, setError] = useState(null);
+    //Avoid infinte for useEffect reference type data, by using useRef
+    const query = useRef(_query).current;
 
     // useEFFECT
     useEffect(() => {
         //Real-time listening to our firestore
         let ref = projectFirestore.collection(collection);
+
+        if(query){
+            ref = ref.where(...query)
+        }
+
         const unsubscribe = ref.onSnapshot((snapshot) => {
             let results = [];
             snapshot.docs.forEach(doc => results.push({...doc.data(), id : doc.id}))
@@ -25,7 +32,7 @@ export const useCollection = (collection) => {
         // cleanup function
         return () => unsubscribe();
 
-    }, [collection]);
+    }, [collection, query]);
 
 
     return { documents, error };
